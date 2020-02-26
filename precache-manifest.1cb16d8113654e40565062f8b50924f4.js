1d64d7e3504acfbf0816a2a1e8ecb66653264b57
Reinaldo Amaran <amaranreinaldo@gmail.com>
2020-02-26 16:15:40 -0300

    Updates

---
+++ precache-manifest.1cb16d8113654e40565062f8b50924f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4346b59d87820b56353a5eaf28751e61",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "34fad750cf8f73ee6197",
+    "url": "/portfolio/static/js/2.6e64dce4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.6e64dce4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6875e06421f8921380d9",
+    "url": "/portfolio/static/js/main.5a0884d5.chunk.js"
+  },
+  {
+    "revision": "1a95985286676740b556",
+    "url": "/portfolio/static/js/runtime-main.f6eb50f0.js"
+  }
+]);
